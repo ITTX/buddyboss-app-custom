fb25021d91900c9b1ea76a2598ab203b992ca33b
Marcin Galczynski <marcin@galczynski.pl>
2024-12-16 14:12:57 +0100

    initial add ms-reporting

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import { initialize } from "@microsoft/react-native-clarity";
+
 export const applyCustomCode = (externalCodeSetup: any) => {
 	// call custom code api here
+	initialize("pchrmrr3vi");
 };
