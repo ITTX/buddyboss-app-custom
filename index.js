8d55b51307a85dbc69488343baf64bb0703292c0
Marcin Galczynski <marcin@galczynski.pl>
2024-12-17 23:14:08 +0100

    update index

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-import { initialize } from "@microsoft/react-native-clarity";
+import React from "react";
+import {View, Text} from "react-native";
 
-export const applyCustomCode = (externalCodeSetup: any) => {
-	// call custom code api here
-	initialize("pchrmrr3vi");
+export const applyCustomCode = externalCodeSetup => {
+    // call custom code api here
+    // externalCodeSetup.configApi.setAppSwitchEnabled(true);
+    externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
+        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Text>This is my signup screen.</Text>
+        </View>
+    ));
+
+	// display at console first load
+    console.log("Log Testing");
+ 
+    // display object in console
+    setTimeout(function(){
+         
+        console.log("This is a console log with object.", {test:1,buddyboss:true});
+ 
+    },1000);
 };
